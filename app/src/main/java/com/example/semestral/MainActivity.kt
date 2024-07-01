6efac4d4b40014471f828ac5a442001d962103f5
Javier Hernandez <javier.hernandez4@utp.ac.pa>
2024-07-01 17:13:30 -0500

    Archivo base para el semestral

---
+++ app/src/main/java/com/example/semestral/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.semestral
+
+import android.annotation.SuppressLint
+
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+}
+
